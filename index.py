72df0e12631cf4f434b48e37a8db408c0946f089
Yash Kathe <yashkathe123@gmail.com>
2023-01-23 05:41:00 +0000

    google books api code attached

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import RPi.GPIO as GPIO
 from pymongo import MongoClient
+import urllib.request
+import json
+import textwrap
 import os
 
 cluster = MongoClient(os.environ.get("MONGO_URI"))
@@ -49,6 +52,28 @@ def qrLogin():
     post = {"email": "yash123", "pid": "123", "password": "123"}
     collection.insert_one(post)
     return render_template('qrlogin.html')
+
+
+@app.route("/student/store-book")
+def booksApi():
+    base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+    with urllib.request.urlopen(base_api_link + "9781451648539") as f:
+        text = f.read()
+    decoded_text = text.decode("utf-8")
+    # deserializes decoded_text to a Python object
+    obj = json.loads(decoded_text)
+    volume_info = obj["items"][0]
+    authors = obj["items"][0]["volumeInfo"]["authors"]
+    publisher = obj["items"][0]["volumeInfo"]["publisher"]
+    image = obj["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+
+    title = volume_info["volumeInfo"]["title"]
+    description = textwrap.fill(
+        volume_info["searchInfo"]["textSnippet"], width=65)
+    pageCount = volume_info["volumeInfo"]["pageCount"]
+    language = volume_info["volumeInfo"]["language"]
+
+    return render_template('store-book.html', title=title, description=description, authors=authors, pageCount=pageCount, language=language, publisher=publisher, image=image)
 
 # Admin pages route
 
---
+++ sensor-scripts/google-books-api.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+import textwrap
+
+base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+user_input = input("Enter ISBN: ").strip()
+with urllib.request.urlopen(base_api_link + user_input) as f:
+    text = f.read()
+decoded_text = text.decode("utf-8")
+obj = json.loads(decoded_text)  # deserializes decoded_text to a Python object
+volume_info = obj["items"][0]
+authors = obj["items"][0]["volumeInfo"]["authors"]
+publisher = obj["items"][0]["volumeInfo"]["publisher"]
+image = obj["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+
+title = volume_info["volumeInfo"]["title"]
+description = textwrap.fill(volume_info["searchInfo"]["textSnippet"], width=65)
+pageCount = volume_info["volumeInfo"]["pageCount"]
+language = volume_info["volumeInfo"]["language"]
+
+print(title, description, authors, pageCount, language, publisher, image)
